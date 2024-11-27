7949a26c736ca8d0d6a05a118bf29c52c697c519
zhiyuan <guoshilei.gsl@antgroup.com>
2024-11-27 18:44:39 +0800

    feat: add hclog

---
+++ pkg/log/hclog.go
@@ -0,0 +1,58 @@
+package log
+
+import (
+	"context"
+	"fmt"
+	"path/filepath"
+
+	"github.com/hashicorp/go-hclog"
+	"google.golang.org/grpc/metadata"
+	"gopkg.in/natefinch/lumberjack.v2"
+
+	"kusionstack.io/kusion-module-framework/pkg/util/kfile"
+)
+
+const (
+	KusionModuleName = "KUSION_MODULE_NAME"
+	KusionTraceID    = "KUSION_TRACE_ID"
+)
+
+func GetModuleLogger(ctx context.Context) hclog.Logger {
+	traceID := getTraceID(ctx)
+	moduleName := getModuleName(ctx)
+	if moduleName == "" {
+		moduleName = "default"
+	}
+
+	kusionDataDir, _ := kfile.KusionDataFolder()
+	logFile := filepath.Join(kusionDataDir, Folder, "modules", moduleName, fmt.Sprintf("%s.log", moduleName))
+	lumberjackLogger := &lumberjack.Logger{
+		Filename:  logFile,
+		MaxSize:   10,
+		Compress:  false,
+		LocalTime: true,
+		MaxAge:    28,
+	}
+
+	return hclog.New(&hclog.LoggerOptions{
+		Name:   moduleName,
+		Output: lumberjackLogger,
+		Level:  hclog.Debug,
+	}).With("trace_id", traceID)
+}
+
+func getTraceID(ctx context.Context) string {
+	ids := metadata.ValueFromIncomingContext(ctx, KusionTraceID)
+	if len(ids) == 0 {
+		return ""
+	}
+	return ids[0]
+}
+
+func getModuleName(ctx context.Context) string {
+	names := metadata.ValueFromIncomingContext(ctx, KusionModuleName)
+	if len(names) == 0 {
+		return ""
+	}
+	return names[0]
+}
